3bd776729873df5c4ac6c7569e08706f1815dee0
ThasneemAzeez <thasneemazeez2001@gmail.com>
2024-06-08 01:00:23 +0530

    Photo app front page created

---
+++ src/components/FrontPage.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const FrontPage = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <h1><center>Taking pictures is savoring life intensely, every hundredth of a second.</center></h1>
+
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <div class="card" >
+                                    <img src="https://media.istockphoto.com/id/480118358/photo/photos-on-a-table.jpg?s=612x612&w=0&k=20&c=hNImlms1Edcc19-piLvf9RIdpEC_P4EF46309bz2BSY=" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <p class="card-text">Photography is a love affair with life</p>
+                                        </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FrontPage
